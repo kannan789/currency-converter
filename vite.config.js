5d6f5f1bdc485b39cb0acc51bcf238b62130a908
Kannan Duraisamy <currentkannan@gmail.com>
2026-07-13 07:06:36 +0530

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  "proxy": "https://api.api-ninjas.com",
+  proxy: "https://api.api-ninjas.com",
+  base: "/currency-converter/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
